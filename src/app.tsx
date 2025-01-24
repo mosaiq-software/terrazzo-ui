177194175315140a5c205f0776803b67a79eeef5
Ternt <kyle.pham202@gmail.com>
2025-01-24 04:20:43 -0500

    temporary work to make the navbar responsive

--- src/app.tsx
+++ src/app.tsx
@@ -1,36 +1,40 @@
 //Utility
 import React from "react";
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
-import { createTheme, MantineProvider } from "@mantine/core";
-import "@mantine/core/styles.css";
+
 import { TRZProvider } from "@trz/util/TRZ-context";
 import { getGithubLoginUrl } from "@trz/util/githubAuth";
 import { GithubAuth } from "@trz/pages/auth/github";
 import { Dashboard } from "@trz/pages/dashboard";
 
-//Components
-import Navbar from "./components/Navbar/Navbar";
-import Board from "./components/Board/Board";
+import { createTheme, MantineProvider, Button, Flex } from "@mantine/core";
+import "@mantine/core/styles.css";
+
+import TRZNavBar from "./components/NavBar"
 
 //Styling
-
-const theme = createTheme({});
+const theme = createTheme({
+    focusRing: "auto",
+    fontSmoothing: true,
+    colors: {
+        'greyscale': ['#101010', '#1F1F1F', '#404040', '#505050', '#656565', '#808080', '#ACACAC', '#DEDEDE', '#F0F0F0', '#FFFFFF'],
+    }
+});
 
 const App = () => {
 	return (
-		<MantineProvider theme={theme}>
+		<MantineProvider defaultColorScheme="auto" theme={theme}>
 			<TRZProvider>
 				<BrowserRouter>
-				<Navbar/>
-					<Routes>
-						<Route path='/' element={<Outlet />} />
-						<Route path='/login' element={<p><a href={getGithubLoginUrl()}>Login with GitHub</a></p>} />
-						<Route path='/auth' element={<Outlet />}>
-							<Route path='github' element={<GithubAuth />} />
-						</Route>
-						<Route path='/dashboard' element={<Dashboard />} />
-						<Route path='/boards/:board_id' element={<Board/>}></Route>
-					</Routes>
+                    <TRZNavBar/>
+                    <Routes>
+                        <Route path='/' element={<Outlet />} />
+                        <Route path='/login' element={<p><a href={getGithubLoginUrl()}>Login with GitHub</a></p>} />
+                        <Route path='/auth' element={<Outlet />}>
+                            <Route path='github' element={<GithubAuth />} />
+                        </Route>
+                        <Route path='/dashboard' element={<Dashboard />} />
+                    </Routes>
 				</BrowserRouter>
 			</TRZProvider>
 		</MantineProvider>
