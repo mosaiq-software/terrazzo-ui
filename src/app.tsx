ce9e7c5beb2b7fe9a15990272a5a9c8707a01608
Matt <47070087+Camo651@users.noreply.github.com>
2025-01-28 14:20:45 -0500

    Socket test

--- src/app.tsx
+++ src/app.tsx
@@ -11,12 +11,42 @@ import { Dashboard } from "@trz/pages/dashboard";
 //Components
 import Navbar from "./components/Navbar/Navbar";
 import Board from "./components/Board/Board";
+import { io } from "socket.io-client";
 
 //Styling
 
+
+
 const theme = createTheme({});
 
+
 const App = () => {
+	React.useEffect(() => {
+		const socketAPIURL = "http://localhost:3002";
+		const socket = io(socketAPIURL, { transports: ['websocket'] });
+	
+		const handleMouseMove = (event: MouseEvent) => {
+			const userData = { id: socket.id, x: event.clientX, y: event.clientY };
+	
+			socket.emit('mousePosition', userData);
+		};
+	
+		window.addEventListener('mousemove', handleMouseMove);
+	
+		socket.on('mousePosition', (userData) => {
+			console.log('mousePosition event triggered with userData: ', userData);
+		});
+	
+		socket.on('userDisconnected', (userId) => {
+			console.log('userDisconnected event triggered with userId: ', userId);
+		});
+	
+		return () => {
+			window.removeEventListener('mousemove', handleMouseMove);
+			socket.disconnect();
+		};
+	}, []);
+
 	return (
 		<MantineProvider theme={theme}>
 			<TRZProvider>
