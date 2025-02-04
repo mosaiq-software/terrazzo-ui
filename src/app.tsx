633e30e6b06b44b82eda79809bc7eb4dc577cb6c
Jonathan Crisanto <tata_cha@outlook.es>
2025-02-03 21:34:46 -0600

    fixed app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,14 @@
 //Utility
 import React from "react";
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
-import { createTheme, MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
 import '@mantine/notifications/styles.css';
 import { createTheme, MantineProvider } from "@mantine/core";
 import { TRZProvider } from "@trz/util/TRZ-context";
-import { getGithubLoginUrl } from "@trz/util/githubAuth";
-import { GithubAuth } from "@trz/pages/auth/github";
-import { Dashboard } from "@trz/pages/dashboard";
 import { SocketProvider } from "@trz/util/socket-context";
 import { Notifications } from '@mantine/notifications';
 import {ModalsProvider} from "@mantine/modals";
 import Router from "./router";
-
-
+import {CreateBoardModal} from "@trz/components/CreateBoard";
 
 const theme = createTheme({});
 
