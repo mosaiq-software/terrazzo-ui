0c0168bceeb0b8406b5471591886d5ffa8dee156
Jonathan Crisanto <tata_cha@outlook.es>
2025-02-05 16:09:37 -0600

    Finished the home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,43 +1,91 @@
 //Utility
 import React from "react";
 //Components
-import {Box, Group, Paper, Text} from "@mantine/core";
+import {Box, Group, Paper, Text, ScrollArea, Title, Flex, UnstyledButton, Divider} from "@mantine/core";
 
 const Home = (): React.JSX.Element => {
     return (
-        <Box bg='#1d2022'
-             h='100vh'
-             w='100vw'
-             p='lg'>
-            <HomeList/>
-            <HomeList/>
-            <HomeList/>
-            <HomeList/>
-        </Box>
+        <ScrollArea bg='#1d2022'
+                    h='100vh'>
+            <Box w='100vw'
+                 pl='15vh'
+                 mih='100vh'>
+                <HomeList/>
+                <Divider mt='40'/>
+                <HomeList/>
+                <Divider mt='40'/>
+                <HomeList/>
+                <Divider mt='40'/>
+                <HomeList/>
+                <Divider mt='40'/>
+                <Divider mt='40'/>
+            </Box>
+        </ScrollArea>
     );
 }
 
 const HomeList = (): React.JSX.Element => {
     return (
-            <Group bg='gray'
-                   m='lg'>
+        <>
+            <Group pt='50'>
+                <Title order={2}
+                       c='white'
+                       p='10'
+                       pr='61vw'>
+                    Your Boards</Title>
+                <Flex justify='flex-end'
+                      gap='3em'
+                      c='white'>
+                    <Text>Boards</Text>
+                    <Text>Members(10)</Text>
+                    <Text>Settings</Text>
+                </Flex>
+            </Group>
+            <Group w='87vw'>
+                <AddHomeListCard/>
+                <HomeListCard/>
                 <HomeListCard/>
                 <HomeListCard/>
                 <HomeListCard/>
                 <HomeListCard/>
             </Group>
+        </>
     );
 }
 
 const HomeListCard = (): React.JSX.Element => {
     return (
-        <Paper bg='#121314'
-               w='20vw'
-               h='175'>
-            <Text c='white'
-                  p='10'>
-                [BRD] Board Name</Text>
-        </Paper>
+        <UnstyledButton m='10'>
+            <Paper bg='#121314'
+                   w='20vw'
+                   h='175'
+                   radius='md'>
+                <Text c='white'
+                      p='10'>
+                    [BRD] Board Name</Text>
+            </Paper>
+        </UnstyledButton>
+    );
+}
+
+const AddHomeListCard = (): React.JSX.Element => {
+    return (
+        <UnstyledButton m='10'>
+            <Paper bg='#121314'
+                   w='20vw'
+                   h='175'
+                   radius='md'>
+                <Flex justify='center'
+                      align='center'
+                      h='100%'
+                      w='100%'>
+                    <Text c='white'
+                          fw={700}>
+                        + Create New Board
+                    </Text>
+                </Flex>
+            </Paper>
+        </UnstyledButton>
     );
 }
 
