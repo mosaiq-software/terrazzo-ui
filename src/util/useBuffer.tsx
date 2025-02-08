347591d4d1b131d7ed40e5686c241255c3de5a0a
Matt <47070087+Camo651@users.noreply.github.com>
2025-02-08 00:31:43 -0500

    useBuffer hook

---
+++ src/util/useBuffer.tsx
@@ -0,0 +1,27 @@
+import { TextBlockEvent } from '@mosaiq/terrazzo-common/types';
+import { useState } from 'react';
+
+function useBuffer<T>() {
+    const [buffer, setBuffer] = useState<T[]>([]);
+
+    const drain = (): T[] => {
+        const b = buffer;
+        setBuffer([]);
+        return b;
+    };
+
+    const push = (item:T): void => {
+        setBuffer([...buffer, item]);
+    };
+
+    const pushMany = (items:T[]): void => {
+        setBuffer([...buffer, ...items]);
+    };
+
+    return {
+        drain,
+        push,
+        pushMany,
+        buffer
+    }
+};
