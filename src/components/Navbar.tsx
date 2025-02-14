d66e872a360adb0e593dcd1dfa8e59dc15c11907
Ternt <kyle.pham202@gmail.com>
2025-02-14 04:10:06 -0500

    was experimenting and exploring ways of styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,10 @@
 import React from "react"
 import { Slot } from "@radix-ui/react-slot"
-import { Title, ActionIcon, Button, Menu } from "@mantine/core"
+import { Title, Text } from "@mantine/core"
 import { useViewportSize } from "@mantine/hooks"
 
 import "../styles/Navbar.css"
-import Autocomplete from "./Autocomplete"
 import { r } from "react-router/dist/production/route-data-DuV3tXo2"
-
 
 // Custom hook that encapsulates a components behaviour 
 const useNavbar = () => {
@@ -22,47 +20,30 @@ const useNavbar = () => {
     }
 }
 
+type NavbarType = {
+    children?: React.ReactNode;
+}
 
 const Navbar = () => {
-    const [value, setValue] = React.useState("");
-
     return (
         <nav className="trzNavigationMenuRoot">
             <ul className="trzNavigationMenuList">
-                <li className="trzNavigationMenuItem" id="trzLogo">
-                    <NavbarTrigger asChild>
-                        <Button
-                            variant="unstyled">
-                            <Title order={3}>Terrazzo</Title>
-                        </Button>
-                    </NavbarTrigger>
+                <li className="trzNavigationMenuItem">
+                    <Title order={1}>Terrazzo</Title>
+                </li>
+                <li className="trzNavigationMenuItem">
+                    <NavbarTrigger>Workspace</NavbarTrigger>
+                </li>
+                <li className="trzNavigationMenuItem">
+                    <NavbarTrigger>Create</NavbarTrigger>
                 </li>
             </ul>
             <ul className="trzNavigationMenuList">
                 <li className="trzNavigationMenuItem">
-                    <Autocomplete
-                        id="trzAutocomplete"
-                        placeholder="Search"
-                        value={value}
-                        onChange={setValue}/>
+                    <NavbarTrigger>Search</NavbarTrigger>
                 </li>
                 <li className="trzNavigationMenuItem">
-                    <NavbarTrigger asChild>
-                        <Menu withArrow>
-                            <Menu.Target>
-                                <ActionIcon 
-                                    variant="unstyled"
-                                    size="lg"
-                                    radius="100"
-                                    aria-label="Account">
-                                    T
-                                </ActionIcon>
-                            </Menu.Target>
-                            <Menu.Dropdown>
-                                <Menu.Label>Account</Menu.Label>
-                            </Menu.Dropdown>
-                        </Menu>
-                    </NavbarTrigger>
+                    <NavbarTrigger>Account</NavbarTrigger>
                 </li>
             </ul>
         </nav>
@@ -71,13 +52,14 @@ const Navbar = () => {
 
 
 type AsChildProp<DefaultElementProps> = 
-  |  ({ asChild?: false } & DefaultElementProps) 
-  |   { asChild: true; children: React.ReactNode}
+    ({ asChild?: false } & DefaultElementProps) 
+  |  { asChild: true; children: React.ReactNode }
 
 type TriggerProp = AsChildProp<React.ButtonHTMLAttributes<HTMLButtonElement>>
 
 const NavbarTrigger = ({asChild, ...props} : TriggerProp) => {
     const Component = asChild ? Slot : "button"
+
     return <Component className="trzNavigationMenuTrigger" {...props}/> 
 }
 
