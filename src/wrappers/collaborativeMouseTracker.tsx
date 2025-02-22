d863795f08f9da725ff1b23988455ed45ca9221c
Matt <47070087+Camo651@users.noreply.github.com>
2025-02-22 13:23:26 -0500

    proper event prop on drag

--- src/wrappers/collaborativeMouseTracker.tsx
+++ src/wrappers/collaborativeMouseTracker.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react';
-import { useMouse } from '@mantine/hooks';
+import React, { MouseEventHandler, useEffect, useRef, useState } from 'react';
 import { useSocket } from "@trz/util/socket-context";
 import UserCursor from "@trz/components/UserCursor";
 import { getRoomCode } from '@mosaiq/terrazzo-common/utils/socketUtils';
@@ -12,12 +11,8 @@ interface CollaborativeMouseTrackerProps {
     children?: any;
 }
 const CollaborativeMouseTracker = (props: CollaborativeMouseTrackerProps) => {
-    const { ref, x, y } = useMouse();
+    const ref = useRef<HTMLDivElement | null>(null);
     const sockCtx = useSocket();
-
-    useEffect(() => {
-        moveMouse();
-    }, [x, y, ref.current]);
 
     useEffect(() => {
         if (!sockCtx.connected) { return; }
@@ -27,12 +22,14 @@ const CollaborativeMouseTracker = (props: CollaborativeMouseTrackerProps) => {
         }
     }, [props.boardId, sockCtx.connected]);
 
-    const moveMouse = () => {
-        const bounds = ref.current.getBoundingClientRect();
-        if(!bounds) return;
+    const moveMouse: MouseEventHandler<HTMLDivElement> = (event) => {
+        if (!ref.current) {return;}
+        const rect = event.currentTarget.getBoundingClientRect();
+        const x = Math.max(0, Math.round(event.pageX - rect.left - (window.pageXOffset || window.scrollX)));
+        const y = Math.max(0, Math.round(event.pageY - rect.top - (window.pageYOffset || window.scrollY)));
         sockCtx.moveMouse({
-            x: x / bounds.width,
-            y: y / bounds.height
+            x: x,
+            y: y
         });
     }
 
@@ -40,6 +37,7 @@ const CollaborativeMouseTracker = (props: CollaborativeMouseTrackerProps) => {
         <Box
             style={props.style}
             ref={ref}
+            onMouseMove={moveMouse}
         >
             {props.children}
             {
@@ -49,8 +47,8 @@ const CollaborativeMouseTracker = (props: CollaborativeMouseTrackerProps) => {
                         <UserCursor
                             key={user.sid}
                             position={{
-                                x: user.mouseRoomData.pos.x * (ref.current?.getBoundingClientRect().width ?? 0) + (ref.current?.getBoundingClientRect().left ?? 0),
-                                y: user.mouseRoomData.pos.y * (ref.current?.getBoundingClientRect().height ?? 0) + (ref.current?.getBoundingClientRect().top ?? 0)
+                                x: user.mouseRoomData.pos.x + (ref.current?.getBoundingClientRect().left ?? 0),
+                                y: user.mouseRoomData.pos.y + (ref.current?.getBoundingClientRect().top ?? 0)
                             }}
                             name={user.fullName}
                             avatarUrl={user.avatarUrl}
