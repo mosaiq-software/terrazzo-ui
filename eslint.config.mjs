8dc3dd7c69928cd353d83c0943067d51d3c26c3b
Jonathan Crisanto <tata_cha@outlook.es>
2025-03-05 00:30:54 -0600

    Lint fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -24,6 +24,6 @@ export default [
         }
     },
     {
-        ignores: [".node_modules/*", "dist/*", "scripts/*", "*.cjs"]
+        ignores: [".node_modules/*", "dist/*", "scripts/*", "*.cjs", "public/*"]
     },
 ];
