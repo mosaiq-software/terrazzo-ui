78c3658290b2d16d2abc684144df03bb3080d192
Matt <47070087+Camo651@users.noreply.github.com>
2025-06-11 18:46:07 -0400

    Hold down button for confirming an action

---
+++ src/components/RingHoldingButton.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import {UnstyledButtonProps, Center, RingProgress, UnstyledButton} from "@mantine/core";
+import { useInterval } from "@mantine/hooks";
+
+interface RingHoldingButtonProps extends UnstyledButtonProps {
+    children?: any;
+    durationMs: number;
+    color?:string;
+    onClick?: ()=>void;
+    onEarlyRelease?: ()=>void;
+    increment?:number;
+    ringSize?:number;
+    ringThickness?:number;
+}
+const DEFAULT_INCREMENT = 1000/60; // 60fps
+export const RingHoldingButton = (props:RingHoldingButtonProps)=>{
+    const [holding, setHolding] = useState<boolean>(false);
+    const [progress, setProgress] = useState<number>(0);
+
+    const onComplete = ()=>{
+        if(props.onClick)
+            props.onClick();
+    }
+
+    const release = ()=>{
+        if(holding && progress < props.durationMs && props.onEarlyRelease){
+            props.onEarlyRelease();
+        }
+        setHolding(false);
+    }
+
+    const down = ()=>{
+        setHolding(true);
+    }
+
+    const interval = useInterval(() => {
+        if(holding){
+            if(progress >= props.durationMs){
+                onComplete();
+            }
+            setProgress((s) => Math.min(s + (props.increment || DEFAULT_INCREMENT), props.durationMs))
+        } else if(progress > 0){
+            setProgress((s) => Math.max(0, Math.pow(s, .95) - 3))
+        }
+    }, (props.increment || DEFAULT_INCREMENT));
+
+    useEffect(() => {
+        interval.start();
+        return interval.stop;
+    }, []);
+
+
+    return (
+        <UnstyledButton
+            {...props}
+            onMouseUp={release}
+            onMouseLeave={release}
+            onMouseDown={down}
+            onClick={(e)=>e.preventDefault()}
+        >
+            <RingProgress
+                size={props.ringSize}
+                thickness={props.ringThickness}
+                sections={[{ value: 100*(progress/props.durationMs), color: props.color ?? "white" }]}
+                label={<Center>{props.children}</Center>}
+            />
+        </UnstyledButton>
+    )
+}
